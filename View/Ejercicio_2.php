10a16b194d651ed9404db752956ac64abd05ed4b
Jenifer Nossa <jpnossa@uniboyaca.edu.co>
2023-10-08 20:13:39 -0500

    Se modifico el ejercicio 2

--- View/Ejercicio_2.php
+++ View/Ejercicio_2.php
@@ -1,32 +1,44 @@
 <?php
-    session_start();
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $figura = $_POST['figura'];
-        $resultado = 0;
-        
-        switch ($figura) {
-            case 'circulo':
-                $radio = floatval($_POST['radio']);
-                $resultado = ($radio ** 2) * M_PI;
-                break;
-            case 'cuadrado':
-                $lado = $_POST['lado'];
-                $resultado = $lado * $lado;
-                break;
-            case 'triangulo':
-                $altura = $_POST['altura'];
-                $base = $_POST['base'];
-                $resultado = ($base * $altura) / 2;
-                break;
-            case 'rectangulo':
-                $altura = $_POST['altura'];
-                $base = $_POST['base'];
-                $resultado = $base * $altura;
-                break;
-        }
-        
-        $_SESSION['resultado'] = $resultado;
+session_start();
+
+$figura = isset($_POST['figura']) ? $_POST['figura'] : '';
+$resultado = 0;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    switch ($figura) {
+        case 'circulo':
+            $radio = isset($_POST['radio']) ? validarNumero($_POST['radio']) : 0;
+            $resultado = ($radio ** 2) * M_PI;
+            break;
+        case 'cuadrado':
+            $lado = isset($_POST['lado']) ? validarNumero($_POST['lado']) : 0;
+            $resultado = $lado * $lado;
+            break;
+        case 'triangulo':
+            $altura = isset($_POST['altura']) ? validarNumero($_POST['altura']) : 0;
+            $base = isset($_POST['base']) ? validarNumero($_POST['base']) : 0;
+            $resultado = ($base * $altura) / 2;
+            break;
+        case 'rectangulo':
+            $altura = isset($_POST['altura']) ? validarNumero($_POST['altura']) : 0;
+            $base = isset($_POST['base']) ? validarNumero($_POST['base']) : 0;
+            $resultado = $base * $altura;
+            break;
     }
+
+    $_SESSION['resultado'] = $resultado;
+}
+
+function validarNumero($valor)
+{
+    // Validar y convertir a float
+    $valor = str_replace(',', '.', $valor); // Reemplazar coma por punto para decimales
+    if (preg_match('/^-?\d+\.?\d*$/', $valor)) {
+        return floatval($valor);
+    }
+    return 0; // Valor predeterminado si no es un número válido
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -43,29 +55,33 @@
     <div class="container">
         <form action="" method="post">
             <label class="radio-label">
-                <input type="radio" name="figura" value="circulo" checked> Circulo
+                <input type="radio" name="figura" value="circulo" <?php echo ($figura === 'circulo') ? 'checked' : ''; ?>> Circulo
             </label>
             <label class="radio-label">
-                <input type="radio" name="figura" value="cuadrado"> Cuadrado
+                <input type="radio" name="figura" value="cuadrado" <?php echo ($figura === 'cuadrado') ? 'checked' : ''; ?>> Cuadrado
             </label>
             <label class="radio-label">
-                <input type="radio" name="figura" value="triangulo"> Triangulo
+                <input type="radio" name="figura" value="triangulo" <?php echo ($figura === 'triangulo') ? 'checked' : ''; ?>> Triangulo
             </label>
-             <label class="radio-label">
-                <input type="radio" name="figura" value="rectangulo"> Rectangulo
+            <label class="radio-label">
+                <input type="radio" name="figura" value="rectangulo" <?php echo ($figura === 'rectangulo') ? 'checked' : ''; ?>> Rectangulo
             </label>
             <br />
-            <label for="radio">Radio</label>
-            <input type="number" id="radio" name="radio" />
-            <br />
-            <label for="lado">Lado</label>
-            <input type="number" id="lado" name="lado" />
-            <br />
-            <label for="base">Base</label>
-            <input type="number" id="base" name="base" />
-            <br />
-            <label for="altura">Altura</label>
-            <input type="number" id="altura" name="altura" />
+
+            <?php if ($figura === 'circulo') : ?>
+                <label for="radio">Radio</label>
+                <input type="text" id="radio" name="radio" />
+            <?php elseif ($figura === 'cuadrado') : ?>
+                <label for="lado">Lado</label>
+                <input type="text" id="lado" name="lado" />
+            <?php elseif ($figura === 'triangulo' || $figura === 'rectangulo') : ?>
+                <label for="base">Base</label>
+                <input type="text" id="base" name="base" />
+                <br />
+                <label for="altura">Altura</label>
+                <input type="text" id="altura" name="altura" />
+            <?php endif; ?>
+
             <br />
             <input type="submit">
             <?php
@@ -73,12 +89,10 @@
                 echo 'El resultado es ' . $_SESSION['resultado'];
                 unset($_SESSION['resultado']);
             }
-
             ?>
-              <a href="../index.html" class="btn">Volver</a>
+            <a href="../index.html" class="btn">Volver</a>
         </form>
-      </div>
-
+    </div>
 </body>
 
 </html>
