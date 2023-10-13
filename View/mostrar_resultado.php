7cea74d7e71674507aaee2ac9c2f2a8c982acefd
Jenifer Nossa <jpnossa@uniboyaca.edu.co>
2023-10-13 12:33:07 -0500

    correccion calcular promedio en Ejercicio 3

--- View/mostrar_resultado.php
+++ View/mostrar_resultado.php
@@ -6,43 +6,40 @@
 </head>
 <body>
     <?php
+    // Verificar si la solicitud es de tipo POST
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // Obtener datos del formulario
         $materia = $_POST['materia'];
         $cantidad_notas = $_POST['cantidad_notas'];
-        $rango_min = $_POST['rango_min'];
-        $rango_max = $_POST['rango_max'];
         
+        // Inicializar la suma de notas
         $suma_notas = 0;
-        
+
+        // Iterar sobre las notas
         for ($i = 1; $i <= $cantidad_notas; $i++) {
+            // Obtener la nota actual del formulario
             $nota = $_POST["nota{$i}"];
             
-            // Validar que la nota esté dentro del rango
-            if ($nota < $rango_min || $nota > $rango_max) {
-                die("Error: La nota {$i} está fuera del rango de calificación.");
-            }
-            
+            // Sumar la nota al total
             $suma_notas += $nota;
         }
+
+        // Calcular el promedio
+        $promedio = $suma_notas / $cantidad_notas;
         
-            $promedio = $suma_notas / $cantidad_notas;
-            $nota_aprobacion = min($promedio + 0.5, $rango_max);
-
-            echo "Promedio: {$promedio}<br>";
-            echo "Nota de Aprobación: {$nota_aprobacion}<br>";
-
-            echo "Promedio de {$materia}: {$promedio}<br>";
-
-            if ($promedio >= $nota_aprobacion) {
-                echo "¡Felicidades! Has aprobado la materia.";
-            } else {
-                echo "Lo siento, no has aprobado la materia.";
-            }
-
+        // Mostrar el resultado
+        echo "Materia: {$materia}<br>";
+        echo "Promedio actual: {$promedio}<br>";
+        
+        // Verificar si el estudiante aprobó o no
+        if ($promedio >= 6) { // Cambia este valor según tus criterios de aprobación
+            echo "¡Felicidades! Has aprobado la materia.";
+        } else {
+            echo "Lo siento, no has aprobado la materia.";
+        }
     }
     ?>
     <a href="Ejercicio3.php" class="btn">Volver</a>
 </body>
 </html>
-
 
